8a8047236f84e496786faa8c850e69b1e8f75eed
ardithyseni <ah49393@ubt-uni.net>
2022-03-14 17:16:58 +0100

    added date time picker

--- src/components/MeetupCreate/MeetupCreateWizard.vue
+++ src/components/MeetupCreate/MeetupCreateWizard.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="meetup-create-form">
     <div class="current-step is-pulled-right">
-      {{currentStep}} of {{allStepsCount}}
+      {{ currentStep }} of {{ allStepsCount }}
     </div>
     <!-- Form Steps -->
     <keep-alive>
-        <MeetupLocation v-if="currentStep === 1" 
-                        @stepUpdated="mergeStepData"
-                        ref="currentComponent" />
-        <MeetupDetail v-if="currentStep === 2" 
-                        @stepUpdated="mergeStepData"
-                        ref="currentComponent" />
-        <MeetupDescription v-if="currentStep === 3" 
-                        @stepUpdated="mergeStepData"
-                        ref="currentComponent" />
-        <MeetupConfirmation v-if="currentStep === 4" :meetupToCreate="form" />
+      <component
+        :is="currentComponent"
+        @stepUpdated="mergeStepData"
+        ref="currentComponent"
+        :meetupToCreate="form"
+      />
     </keep-alive>
 
-    <progress class="progress"
-              :value="currentProgress"
-               max="100">{{currentProgress}}%</progress>
+    <progress class="progress" :value="currentProgress" max="100">
+      {{ currentProgress }}%
+    </progress>
     <div class="controll-btns m-b-md">
-      <button v-if="currentStep !== 1"
-              @click="moveToPreviousStep"
-              class="button is-primary m-r-sm">Back</button>
-      <button v-if="currentStep !== allStepsCount"
-              @click="moveToNextStep"
-              :disabled="!canProceed"
-              class="button is-primary">Next</button>
-      <button v-else
-              class="button is-primary">Confirm</button>
+      <button
+        v-if="currentStep !== 1"
+        @click="moveToPreviousStep"
+        class="button is-primary m-r-sm"
+      >
+        Back
+      </button>
+      <button
+        v-if="currentStep !== allStepsCount"
+        @click="moveToNextStep"
+        :disabled="!canProceed"
+        class="button is-primary"
+      >
+        Next
+      </button>
+      <button v-else class="button is-primary">Confirm</button>
     </div>
     <!-- Just To See Data in the Form -->
     <pre><code>{{form}}</code></pre>
@@ -37,70 +40,82 @@
 </template>
 
 <script>
-  import MeetupLocation from './MeetupLocation'
-  import MeetupDetail from './MeetupDetail'
-  import MeetupDescription from './MeetupDescription'
-  import MeetupConfirmation from './MeetupConfirmation'
-  export default {
-    components: {
-      MeetupLocation,
-      MeetupDetail,
-      MeetupDescription,
-      MeetupConfirmation
-    },
-    data () {
-      return {
-        currentStep: 1,
-        allStepsCount: 4,
-        canProceed: false,
-        form: {
-          location: null,
-          title: null,
-          startDate: null,
-          category: null,
-          image: null,
-          shortInfo: null,
-          description: null,
-          timeTo: null,
-          timeFrom: null
-        }
-      }
-    },
-    computed: {
-      currentProgress () {
-        return (100 / this.allStepsCount) * this.currentStep
-      }
-    },
-    methods: {
-      mergeStepData (step) {
-        this.form = {...this.form, ...step.data}
-        this.canProceed = step.isValid
+import MeetupLocation from "./MeetupLocation";
+import MeetupDetail from "./MeetupDetail";
+import MeetupDescription from "./MeetupDescription";
+import MeetupConfirmation from "./MeetupConfirmation";
+export default {
+  components: {
+    MeetupLocation,
+    MeetupDetail,
+    MeetupDescription,
+    MeetupConfirmation,
+  },
+  data() {
+    return {
+      currentStep: 1,
+      canProceed: false,
+      formSteps: [
+        "MeetupLocation",
+        "MeetupDetail",
+        "MeetupDescription",
+        "MeetupConfirmation",
+      ],
+      form: {
+        location: null,
+        title: null,
+        startDate: null,
+        category: null,
+        image: null,
+        shortInfo: null,
+        description: null,
+        timeTo: null,
+        timeFrom: null,
       },
+    };
+  },
+  computed: {
+    allStepsCount() {
+      return this.formSteps.length;
+    },
 
-      moveToNextStep () {
-        this.currentStep++
+    currentProgress() {
+      return (100 / this.allStepsCount) * this.currentStep;
+    },
+    currentComponent() {
+      return this.formSteps[this.currentStep - 1];
+    },
+  },
+  methods: {
+    mergeStepData(step) {
+      this.form = { ...this.form, ...step.data };
+      this.canProceed = step.isValid;
+    },
 
-        // https://vuejs.org/v2/api/#Vue-nextTick
-        // Defer the callback to be executed after the next DOM update cycle.
-        this.$nextTick(() => {
-          this.canProceed = !this.$refs['currentComponent'].$v.$invalid
-        })
-      },
-      moveToPreviousStep () {
-        this.currentStep--
-        this.canProceed = true
-      }
-    }
-  }
+    moveToNextStep() {
+      this.currentStep++;
+
+      // https://vuejs.org/v2/api/#Vue-nextTick
+      // Defer the callback to be executed after the next DOM update cycle.
+      this.$nextTick(() => {
+        this.canProceed = !this.$refs["currentComponent"].$v.$invalid;
+      });
+    },
+    moveToPreviousStep() {
+      this.currentStep--;
+      this.canProceed = true;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .meetup-create-form {
-    box-sizing: border-box;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 840px;
-    padding: 24px 16px 8px;
-    width: 100%;
-  }
+.meetup-create-form {
+  box-sizing: border-box;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 840px;
+  padding: 24px 16px 8px;
+  width: 100%;
+}
 </style>
--- src/components/MeetupCreate/MeetupDetail.vue
+++ src/components/MeetupCreate/MeetupDetail.vue
@@ -2,57 +2,70 @@
   <form @input="emitFormData">
     <div class="field">
       <label class="title m-b-sm">Choose Title</label>
-      <input v-model="form.title"
-            @blur="$v.form.title.$touch()"
-             class="input"
-             type="text"
-             placeholder="Enter Title">
+      <input
+        v-model="form.title"
+        @blur="$v.form.title.$touch()"
+        class="input"
+        type="text"
+        placeholder="Enter Title"
+      />
       <div v-if="$v.form.title.$error">
-        <span v-if="!$v.form.title.required" class="help is-danger">Title is required</span>
+        <span v-if="!$v.form.title.required" class="help is-danger"
+          >Title is required</span
+        >
       </div>
     </div>
     <div class="field">
-      <label class="title m-b-sm">Starts At</label>
-      <input v-model="form.startDate"
-            @blur="$v.form.title.$touch()"
-             class="input"
-             type="text"
-             placeholder="Starts At">
+      <label class="title m-b-sm">Start Date</label>
+      <datepicker
+        @input="setDate"
+        :disabledDates="disabledDates"
+        :input-class="'input'"
+        :placeholder="new Date() | formatDate"
+      ></datepicker>
       <div v-if="$v.form.startDate.$error">
-        <span v-if="!$v.form.startDate.required" class="help is-danger">Starts at is required</span>
+        <span v-if="!$v.form.startDate.required" class="help is-danger"
+          >Starts at is required</span
+        >
       </div>
     </div>
     <div class="field">
       <label class="title m-b-sm">From</label>
-      <input v-model="form.timeFrom"
-            @blur="$v.form.timeFrom.$touch()"
-             class="input"
-             type="text"
-             placeholder="Time From">
+      <vue-timepicker
+        :minute-interval="10"
+        @change="changeTime($event, 'timeFrom')"
+      ></vue-timepicker>
     </div>
     <div class="field">
       <label class="title m-b-sm">To</label>
-      <input v-model="form.timeTo"
-            @blur="$v.form.timeFrom.$touch()"
-             class="input"
-             type="text"
-             placeholder="Time to">
+      <vue-timepicker
+        :minute-interval="5"
+        @change="changeTime($event, 'timeTo')"
+      ></vue-timepicker>
     </div>
     <div class="field">
       <label class="title m-b-sm">Please Choose the Category.</label>
       <div class="m-b-lg">
         <div class="select">
-         
-            <select v-model="form.category"
-                    @blur="$v.form.category.$touch()" 
-                    @change="emitFormData">
-             <option v-for="category of categories"
-                    :value="category"
-                    :key="category.id">{{category.name}}</option>
-          </select> 
+          <!-- TODO: Get Here Categories -->
+          <select
+            v-model="form.category"
+            @blur="$v.form.category.$touch()"
+            @change="emitFormData"
+          >
+            <option
+              v-for="category of categories"
+              :value="category"
+              :key="category.id"
+            >
+              {{ category.name }}
+            </option>
+          </select>
         </div>
         <div v-if="$v.form.category.$error">
-          <span v-if="!$v.form.category.required" class="help is-danger">Category is required</span>
+          <span v-if="!$v.form.category.required" class="help is-danger"
+            >Category is required</span
+          >
         </div>
       </div>
     </div>
@@ -60,43 +73,70 @@
 </template>
 
 <script>
-  import { required } from 'vuelidate/lib/validators'
-  export default {
-    data () {
-      return {
-        form: {
-          title: null,
-          startDate: null,
-          timeTo: null,
-          timeFrom: null,
-          category: null
-        }
-      }
-    },
-    validations: {
+import Datepicker from "vuejs-datepicker";
+import VueTimepicker from 'vue2-timepicker/src/vue-timepicker.vue'
+import moment from "moment";
+import { required } from "vuelidate/lib/validators";
+export default {
+  components: {
+    Datepicker,
+    VueTimepicker,
+  },
+  data() {
+    return {
+      disabledDates: {
+        customPredictor: function (date) {
+          const today = new Date();
+          const yesterday = today.setDate(today.getDate() - 1);
+          return date < yesterday;
+        },
+      },
       form: {
-        title: { required },
-        startDate: { required },
-        category: { required },
-        timeTo: { required },
-        timeFrom: { required }
-      }
+        title: null,
+        startDate: null,
+        timeTo: null,
+        timeFrom: null,
+        category: null,
+      },
+    };
+  },
+  validations: {
+    form: {
+      title: { required },
+      startDate: { required },
+      category: { required },
+      timeTo: { required },
+      timeFrom: { required },
     },
-    computed: {
-      categories () {
-        return this.$store.state.categories.items
-      }
+  },
+  computed: {
+    categories() {
+      return this.$store.state.categories.items;
     },
-    methods: {
-      emitFormData () {
-        this.$emit('stepUpdated', {data: this.form, isValid: !this.$v.$invalid})
-      }
-    }
-  }
+  },
+  methods: {
+    emitFormData() {
+      this.$emit("stepUpdated", {
+        data: this.form,
+        isValid: !this.$v.$invalid,
+      });
+    },
+    setDate(date) {
+      this.form.startDate = moment(date).format();
+      this.emitFormData();
+    },
+    changeTime({ data }, field) {
+      const minutes = data.mm || "00";
+      const hours = data.HH || "00";
+      this.form[field] = hours + ":" + minutes;
+      this.emitFormData();
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .time-picker {
-    display: block;
-  }
+.time-picker {
+  display: block;
+}
 </style>
